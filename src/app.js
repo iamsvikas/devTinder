5ca92fc9dc175b094e5e121da0023a0a66cace22
balkar-appricott <104197133+balkar-appricott@users.noreply.github.com>
2025-03-26 22:04:58 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,24 @@ const express = require('express');
 
 const app = express();
 
-app.use('/hello', (req, res) => {
-  res.send('hello!');
+app.get('/user', (req, res) => {
+  res.send('received users successfully!!');
+});
+
+app.post('/user', (req, res) => {
+  res.send('stored data successfully!');
+});
+
+app.delete('/user', (req, res) => {
+  res.send('deleted data successfully!');
+});
+
+app.put('/user', (req, res) => {
+  res.send('updated user successfully!');
+});
+
+app.patch('/user', (req, res) => {
+  res.send('user details updated!');
 });
 app.use('/test', (req, res) => {
   res.send('hello! testing');
