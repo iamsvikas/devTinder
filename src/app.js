a7a643060fdef37deec84dc9f601dc55031afa3c
balkar-appricott <104197133+balkar-appricott@users.noreply.github.com>
2025-04-07 22:02:52 +0530

    authentication using cookies and jwt

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 /** @format */
 
-const express = require("express");
-const connectDB = require("./config/database");
+const express = require('express');
+const connectDB = require('./config/database');
 const app = express();
-const User = require("./models/user");
-const bcrypt = require("bcrypt");
-const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const { validateSignUpData } = require('./utils/validation');
+const { userAuth } = require('./middleware/auth');
+
 // const cors = require("cors");
 // const corsOptions = {
 //   origin: "http://localhost:5173", // Replace with your frontend URL
@@ -18,7 +20,7 @@ const { validateSignUpData } = require('./utils/validation');
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/signup", async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { firstName, lastName, emailId, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log(hashedPassword);
@@ -40,56 +42,59 @@ app.post("/signup", async (req, res) => {
       throw new Error('maximum 10 skills allowed!');
     const user = new User(req.body);
     await user.save();
-    res.send("User added successfully!");
+    res.send('User added successfully!');
   } catch (err) {
     res.status(400).send('ERROR: ' + err.message);
   }
 });
 
-app.post("/login", async (req, res) => {
+app.post('/login', async (req, res) => {
   const { emailId, password } = req.body;
   try {
     const user = await User.findOne({ emailId });
     if (!user) {
-      return res.status(404).send("User not found!");
+      return res.status(404).send('User not found!');
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).send("Invalid credentials!");
+      return res.status(400).send('Invalid credentials!');
     }
-    const token = await jwt.sign({ userId: user._id }, "Dev@tinder1234", {
-      expiresIn: "1h",
+    const token = await jwt.sign({ userId: user._id }, 'Dev@tinder1234', {
+      expiresIn: '1d',
     });
-    res.cookie("token", token);
-    return res.status(200).send("Login successful!");
+    res.cookie('token', token, {
+      expires: new Date(Date.now() + 24 * 3600000),
+    });
+    return res.status(200).send('Login successful!');
   } catch (error) {
     return res.status(400).send(`Error during login: ${error.message}`);
   }
 });
 
-app.get("/profile", async (req, res) => {
+app.get('/profile', userAuth, async (req, res) => {
   try {
-    const cookies = req.cookies;
-    // console.log(cookies);
-    const { token } = cookies;
-    // console.log(token);
-    if (!token) {
-      throw new Error("Unauthorized access!");
-    }
-    const decodedMessage = await jwt.verify(token, "Dev@tinder1234");
-    console.log(decodedMessage);
-    const { userId } = decodedMessage;
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).send("User not found!");
-    }
+    // const cookies = req.cookies;
+    // // console.log(cookies);
+    // const { token } = cookies;
+    // // console.log(token);
+    // if (!token) {
+    //   throw new Error('Unauthorized access!');
+    // }
+    // const decodedMessage = await jwt.verify(token, 'Dev@tinder1234');
+    // console.log(decodedMessage);
+    // const { userId } = decodedMessage;
+    const user = req.user;
+    // if (!user) {
+    //   return res.status(404).send('User not found!');
+    // }
+    console.log(req, user);
     res.send(user);
   } catch (error) {
     return res.status(400).send(`Error: ${error.message}`);
   }
 });
 
-app.get("/user", async (req, res) => {
+app.get('/user', async (req, res) => {
   try {
     const userEmail = req.body.emailId;
     const users = await User.find({ emailId: userEmail });
@@ -97,36 +102,36 @@ app.get("/user", async (req, res) => {
     if (users.length > 0) {
       res.send(users);
     } else {
-      res.status(404).send("User not found!");
+      res.status(404).send('User not found!');
     }
   } catch (err) {
-    res.status(404).send("some error ocurred!");
+    res.status(404).send('some error ocurred!');
   }
 });
-app.get("/feed", async (req, res) => {
+app.get('/feed', async (req, res) => {
   try {
     const users = await User.find();
     if (users.length > 0) {
       res.send(users);
     } else {
-      res.status(404).send("No User found!");
+      res.status(404).send('No User found!');
     }
   } catch (err) {
-    res.status(404).send("some error ocurred!");
+    res.status(404).send('some error ocurred!');
   }
 });
-app.delete("/user", async (req, res) => {
+app.delete('/user', async (req, res) => {
   const userId = await req.body.userId;
   try {
     // const user = await User.findByIdAndDelete({ _id: userId });
     const user = await User.findByIdAndDelete(userId);
-    res.send("User deleted successfully");
+    res.send('User deleted successfully');
   } catch (err) {
-    res.status(404).send("some error ocurred!");
+    res.status(404).send('some error ocurred!');
   }
 });
 
-app.patch("/user", async (req, res) => {
+app.patch('/user', async (req, res) => {
   const userId = req.body.userId;
   const data = req.body;
   if (req.body.skills?.length > 10)
@@ -134,23 +139,23 @@ app.patch("/user", async (req, res) => {
   console.log(data);
   try {
     await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "after",
+      returnDocument: 'after',
       runValidators: true,
     });
-    res.send("User updated successfully");
+    res.send('User updated successfully');
   } catch (err) {
-    res.status(400).send("something went wrong!");
+    res.status(400).send('something went wrong!');
   }
 });
 
 connectDB()
   .then(() => {
-    console.log("Database connection established...");
+    console.log('Database connection established...');
     app.listen(5173, () => {
-      console.log("server is successfully listening on port 5173");
+      console.log('server is successfully listening on port 5173');
     });
   })
-  .catch((err) => console.log("Database connetion failed!"));
+  .catch((err) => console.log('Database connetion failed!'));
 
 /* 
 const mongoose = require('mongoose');
---
+++ src/middleware/auth.js
@@ -0,0 +1,35 @@
+/** @format */
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+// const adminAuth = (req, res, next) => {
+//   const token = 'xyz';
+//   const isAuthorised = token === 'xyz';
+//   if (isAuthorised) {
+//     next();
+//   } else {
+//     res.status(401).send('not authorised !');
+//   }
+// };
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) throw new Error('Token is invalid!');
+    const decodedObj = await jwt.verify(token, 'Dev@tinder1234');
+    const { userId } = decodedObj;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('User not found!');
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send('Error: ' + err.message);
+  }
+};
+
+module.exports = {
+  // adminAuth,
+  userAuth,
+};
